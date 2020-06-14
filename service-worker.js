eb77bd3ca0a625155886848b4f1d277c8764843d
Максим <makc-dmv@yandex.ru>
2020-06-14 14:31:59 +0500

    Update 2020-06-14T09:31:51.655Z

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9a2b45c2d153ad55416ea49e0a45e880.js"
+  "./precache-manifest.115197b577ffe6ea5f341fbb3ad41604.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
